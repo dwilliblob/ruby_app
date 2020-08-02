7dd11b59c9f3d08b1fc38b83044e1159e98947ff
Lee Williams <lee.williams@evesfield.co.uk>
2020-08-02 23:52:54 +0100

    Moved execution script to App.rb helping with code test coverage. Updated README.md to include new instructions for executing program

--- src/loader.rb
+++ src/loader.rb
@@ -17,29 +17,4 @@ class Loader
     }
     return @web_log
   end
-
-  def load_webserver_log_reports
-    input_filename = ARGV[0]
-    logs = Loader.new.parse_file(input_filename)
-    wl = WebLog.new
-
-    most_page_views = wl.most_page_views(logs)
-    puts "-" * 100
-    puts "Ranked most viewed pages"
-    puts "-" * 100
-    most_page_views.each do |k,v|
-      puts "#{k} = #{v}"
-    end
-    puts "-" * 100
-    puts ""
-
-    unique_page_views = wl.unique_page_views(logs)
-    puts "-" * 100
-    puts "Number of unique page views"
-    puts "-" * 100
-    unique_page_views.each do |page_view|
-      puts "#{page_view[0]} = #{page_view[1]}"
-    end
-    puts "-" * 100
-  end
 end
